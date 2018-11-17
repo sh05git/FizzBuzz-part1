b62a353a7b1eab45a3f0e19ccac8f5678ef8b91c
shogo <bttw.1011@gmail.com>
2018-11-17 14:55:07 +0900

    15の倍数ならFizzBuzzと出力するようになった
    if文は上から順に条件に当てはめていくので
    number % 15 == 0を一番上に持っていった。

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,11 +2,12 @@ number = int(input('1つの自然数を入れてね: '))
 
 output = str(number)
 
-if number % 3 == 0:
+if number % 15 == 0:
+    output = 'FizzBuzz'
+elif number % 3 == 0:
     output = 'Fizz'
 elif number % 5 == 0:
     output = 'Buzz'
-
 else:
     output = str(number)
 
